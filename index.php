6321c57217c0a5e66b884410461d38230f6beea5
akprinceinfo <akprince0268@gmail.com>
2024-09-07 23:59:11 +0600

    Problem 20

--- index.php
+++ index.php
@@ -899,6 +899,26 @@ echo test(99, 70)."\n";
 
 ?>
 
+<!-- 20. Write a PHP program to check whether two given integers are in the range 40..50 inclusive, or they are both in the range 50..60 inclusive. -->
+<?php
+// Define a function that checks if a point is within one of two rectangular regions
+function test($x, $y) 
+{
+    $condition1 = ($x >= 40 && $x <= 50 && $y >= 40 && $y <= 50);
+    
+    $condition2 = ($x >= 50 && $x <= 60 && $y >= 50 && $y <= 60);
+
+    return $condition1 || $condition2;
+}
+
+
+    var_dump(test(78, 95));
+    var_dump(test(25, 35));
+    var_dump(test(40, 50));
+    var_dump(test(55, 60));
+    ?>
+
+
 
 </body>
 </html>
